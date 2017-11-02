9ce8e21e0c52d61528c4e0ca3d5cd0f172db9c8d
LYounD <gongja250@naver.com>
2017-11-02 13:01:16 +0900

    ex6

--- gradestore/cheater.php
+++ gradestore/cheater.php
@@ -24,7 +24,7 @@
 		# Check the existance of each parameter using the PHP function 'isset'.
 		# Check the blankness of an element in $_POST by comparing it to the empty string.
 		# (can also use the element itself as a Boolean test!)
-		 if (!isset($name) or !isset($id) or !isset($card) or !isset($credit)){
+		 if (!isset($name) or ($id=='') or !isset($card) or !isset($credit) or ($course=='')){
 		?>
 
 		<!-- Ex 4 : 
@@ -35,20 +35,20 @@
 		<?php
 		# Ex 5 : 
 		# Check if the name is composed of alphabets, dash(-), ora single white space.
-		 } elseif (!preg_match('/^[a-zA-Z]+$/', $name) and ( preg_match('/^[a-zA-Z]+[-]{1}[a-zA-Z]+$/', $name) + preg_match('/^[a-zA-Z]+[\s]{1}[a-zA-Z]+$/', $name) <> 1)) { 
+		 } elseif (!preg_match('/^([a-zA-Z]+[-,\s]{1}){0,}[a-zA-Z]+$/', $name)) {
 		?>
 
 		<!-- Ex 5 : 
 			Display the below error message : -->
 			<h1>Sorry</h1>
-			<p>You didn't provide a valid name. <a href="gradestore.html">Try again?</a></p> 
+			<p>You didn't provide a valid name. <a href="gradestore.html">Try again?</a></p>
 		
 
 		<?php
 		# Ex 5 : 
 		# Check if the credit card number is composed of exactly 16 digits.
 		# Check if the Visa card starts with 4 and MasterCard starts with 5. 
-		 } elseif ( (!preg_match("/\d{16}/", $card)) or ( ( ($credit == 'visa') and !preg_match("/^[4]/", $card) ) or ( ($credit == 'masterCard') and !preg_match("/^[5]/", $card) ) )   ) {
+		 } elseif ( (!preg_match("/^\d{16}$/", $card)) or ( ( ($credit == 'visa') and !preg_match("/^[4]/", $card) ) or ( ($credit == 'mastercard') and !preg_match("/^[5]/", $card) ) )   ) {
 		?>
 
 		<!-- Ex 5 : 
@@ -96,8 +96,7 @@
 		
 		<?php
 			$content = file_get_contents("loosers.txt");
-			$prints = explode("\n", $content);
-		}
+		
 		?>
 		<p>Here are all the loosers who have submitted here:</p>
 
@@ -105,6 +104,7 @@
 <?= $content ?>
 		</pre>
 		
+		<?php } ?>
 		<?php
 			/* Ex 2: 
 			 * Assume that the argument to this function is array of names for the checkboxes ("cse326", "cse107", "cse603", "cin870")
